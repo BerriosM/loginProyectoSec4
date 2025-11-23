e03f307525ab10df41764f13449c211d6852436a
BerriosM <matias.berrios13@inacapmail.cl>
2025-11-23 18:01:44 -0300

    .

--- loginProyecto/settings.py
+++ loginProyecto/settings.py
@@ -74,15 +74,10 @@ WSGI_APPLICATION = 'loginProyecto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-import pymysql
-pymysql.install_as_MySQLdb()
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'loginSec4',
-        'USER':'root',
-        'PASSWORD' : ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
